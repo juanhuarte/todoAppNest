7370406cf01471fbf883ee1f8587c1d3994851ac
Juan Huarte <juan_huarte@hotmail.com>
2022-03-03 11:14:29 -0300

    update changes for deploy

--- api/src/orm.config.ts
+++ api/src/orm.config.ts
@@ -2,13 +2,30 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { config } from 'dotenv';
 config();
 
-export const configuration: TypeOrmModuleOptions = {
-  type: 'postgres',
-  username: process.env.DB_USER, //'postgres',
-  password: process.env.DB_PASSWORD, //'Juanignacio97',
-  host: process.env.DB_HOST, //'localhost',
-  port: 5432,
-  database: process.env.DB_NAME, //'todo',
-  synchronize: true,
-  entities: ['dist/**/*.entity{.ts,.js}'],
-};
+export const configuration: TypeOrmModuleOptions =
+  process.env.NODE_ENV === 'production'
+    ? {
+        type: 'postgres',
+        username: process.env.DB_USER, //'postgres',
+        password: process.env.DB_PASSWORD, //'Juanignacio97',
+        host: process.env.DB_HOST, //'localhost',
+        port: 5432,
+        database: process.env.DB_NAME, //'todo',
+        extra: {
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        synchronize: true,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+      }
+    : {
+        type: 'postgres',
+        username: process.env.DB_USER, //'postgres',
+        password: process.env.DB_PASSWORD, //'Juanignacio97',
+        host: process.env.DB_HOST, //'localhost',
+        port: 5432,
+        database: process.env.DB_NAME, //'todo',
+        synchronize: true,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+      };
